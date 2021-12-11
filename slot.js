6af09e295f3e8393bf2c2cb5ca7c6938be2b55c7
Albert <71594935+BEBXX@users.noreply.github.com>
2021-12-11 11:12:18 +0700

    Add files via upload

---
+++ slot.js
@@ -0,0 +1,115 @@
+const SLOTS_PER_REEL = 12;
+// radius = Math.round( ( panelWidth / 2) / Math.tan( Math.PI / SLOTS_PER_REEL ) ); 
+// current settings give a value of 149, rounded to 150
+const REEL_RADIUS = 150;
+
+function createSlots (ring) {
+	
+	var slotAngle = 360 / SLOTS_PER_REEL;
+	var seed = getSeed();
+	
+
+	for (var i = 0; i < SLOTS_PER_REEL; i ++) {
+		var slot = document.createElement('div');
+		
+		slot.className = 'slot';
+
+		// compute and assign the transform for this slot
+		var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
+
+		slot.style.transform = transform;
+
+		const img_table = {
+			0: "images/seven.png",
+			1: "images/cherry.png",
+			2: "images/lemon.png",
+			3: "images/diamond.png",
+			4: "images/orange.png",
+			5: "images/chip.png",
+			6: "images/seven.png",
+			7: "images/cherry.png",
+			8: "images/lemon.png",
+			9: "images/diamond.png",
+			10: "images/orange.png",
+			11: "images/chip.png"
+		};
+
+		var content = $(slot).append('<p><img src = "' + img_table[((seed + i)%12)] + '" width = "70" height = "70"></p>');
+
+		// add the poster to the row
+		ring.append(slot);
+	}
+}
+
+function getSeed() {
+	// generate random number smaller than 13 then floor it to settle between 0 and 12 inclusive
+	return Math.floor(Math.random()*(SLOTS_PER_REEL));
+}
+
+function spin(timer) {
+	//var txt = 'seeds: ';
+	for(var i = 1; i < 6; i ++) {
+		var oldSeed = -1;
+		/*
+		checking that the old seed from the previous iteration is not the same as the current iteration;
+		if this happens then the reel will not spin at all
+		*/
+		var oldClass = $('#ring'+i).attr('class');
+		if(oldClass.length > 4) {
+			oldSeed = parseInt(oldClass.slice(10));
+			console.log(oldSeed);
+		}
+		var seed = getSeed();
+		while(oldSeed == seed) {
+			seed = getSeed();
+		}
+
+		$('#ring'+i)
+			.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
+			.attr('class','ring spin-' + seed);
+	}
+
+	console.log('=====');
+}
+
+$(document).ready(function() {
+
+	// initiate slots 
+ 	createSlots($('#ring2'));
+ 	createSlots($('#ring3'));
+ 	createSlots($('#ring4'));
+
+ 	// hook start button
+ 	$('.go').on('click',function(){
+ 		var timer = 2;
+ 		spin(timer);
+ 	})
+
+ 	// hook xray checkbox
+ 	$('#xray').on('click',function(){
+ 		//var isChecked = $('#xray:checked');
+ 		var tilt = 'tiltout';
+ 		
+    if($(this).is(':checked')) {
+ 			tilt = 'tiltin';
+ 			$('#rotate').css('animation',tilt + ' 2s 1');
+
+			setTimeout(function(){
+			  $('#rotate').toggleClass('tilted');
+			},2000);
+ 		} else {
+      tilt = 'tiltout';
+ 			$('#rotate').css({'animation':tilt + ' 2s 1'});
+
+			setTimeout(function(){
+	 			$('#rotate').toggleClass('tilted');
+	 			$('.slot').removeClass('backface-on');
+	 		},1900);
+ 		}
+ 	})
+
+ 	// hook perspective
+ 	$('#perspective').on('click',function(){
+ 		$('#stage').toggleClass('perspective-on perspective-off');
+ 	})	
+ });
